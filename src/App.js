7a339ec7872e9493dc8f80bad4f7cd51e0d5798f
bwillstorey <bwillstorey@gmail.com>
2019-06-02 16:13:19 -0500

    adding requirements pseudocode

--- src/App.js
+++ src/App.js
@@ -23,4 +23,18 @@ function App() {
   );
 }
 
+
+// REQUIREMENTS
+
+// The application should render different images (of your choice) to the screen. 
+  // Each image should listen for click events. - CLICK EVENT HANDLERS
+
+// The application should keep track of the user's score. - VARIABLE FOR SCORE, INCREMEMNTING WITH RIGHT ANSWER
+  // The user's score should be incremented when clicking an image for the first time. - 
+  // The user's score should be reset to 0 if they click the same image more than once. - CONDITIONAL (DETERMINE BEST ONE)
+
+// Every time an image is clicked, the images rendered to the page should shuffle themselves in a random order. - MATH RANDOM FORMULA
+
+// Once the user's score is reset after an incorrect guess, the game should restart. - CONDITIONAL IF/ELSE OR SWITCH OR TERNINARY
+
 export default App;
